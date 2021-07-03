05621d427ae98cbaeafdbf6e1c376699eace5023
Siyun Kim <sharon1638@naver.com>
2021-07-04 07:18:13 +0900

    CRUD

--- purchase_mate/posts/urls.py
+++ purchase_mate/posts/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from .views import *
+from django.utils import timezone
 
 app_name = "posts"
 
 urlpatterns = [
-    path("recent_postlist/",recent_postlist , name="recent_postlist"),
-    path("makepost/",makepost , name="makepost"),
-    
+    path("recent_postlist/",recent_postlist,name="recent_postlist"),
+    path("makepost/",makepost,name="makepost"),
+    path("<str:id>",detail,name="detail"),
+    path("create/",create, name="create"),
+    path('edit/<str:id>', edit, name="edit"),
+    path('update/<str:id>',update, name="update"),
+    path('delete/<str:id>', delete, name="delete"),
 ]
