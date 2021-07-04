d4c7e853da5d773f7a3c7d9c9e94c4d86db8b7c5
dddooo9 <dlaehdus99@naver.com>
2021-07-04 14:00:47 +0900

    메인페이지 최근 글 목록 view 추가

--- purchase_mate/purchase_mate/views.py
+++ purchase_mate/purchase_mate/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from posts.models import Post
 
 def showmain(request):
-    return render(request, 'main.html')
+    if Post.objects.count() > 3:
+        posts = Post.objects.all().order_by('-pk')[:4]
+    else:
+        posts = Post.objects.all().order_by('-pk')
+    
+    user = request.user
+    my_posts = Post.objects.filter(writer=user)
+    if my_posts.count() > 3:
+        my_posts = my_posts.order_by('-pk')[:4]
+    return render(request, 'main.html', {'posts':posts, 'my_posts':my_posts})
