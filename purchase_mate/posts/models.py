c500e571b326606ba0dd7ee85ddd331346653745
LeeTaeGeom <taegeom0601@gmail.com>
2021-07-04 11:55:40 +0900

    post에 category추가

--- purchase_mate/posts/models.py
+++ purchase_mate/posts/models.py
@@ -7,9 +7,10 @@ class Post(models.Model):
     id=models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     writer=models.ForeignKey(User,on_delete=models.CASCADE)
-    quantity=models.IntegerField()
-    price=models.IntegerField()
-    people=models.IntegerField(null=True)
+    quantity=models.IntegerField(default=0)
+    price=models.IntegerField(default=0)
+    people=models.IntegerField(default=0, null=True)
+    category=models.CharField(max_length=20)
 
     def __str__(self):
         return self.title
