f5bf414e82b47e0e614eee7190e3dbc11f7f008a
dddooo9 <dlaehdus99@naver.com>
2021-07-04 14:03:26 +0900

    코드 간소화

--- purchase_mate/purchase_mate/views.py
+++ purchase_mate/purchase_mate/views.py
@@ -2,13 +2,7 @@ from django.shortcuts import render
 from posts.models import Post
 
 def showmain(request):
-    if Post.objects.count() > 3:
-        posts = Post.objects.all().order_by('-pk')[:4]
-    else:
-        posts = Post.objects.all().order_by('-pk')
-    
+    posts = Post.objects.all().order_by('-pk')[:4]
     user = request.user
-    my_posts = Post.objects.filter(writer=user)
-    if my_posts.count() > 3:
-        my_posts = my_posts.order_by('-pk')[:4]
+    my_posts = Post.objects.filter(writer=user).order_by('-pk')[:4]
     return render(request, 'main.html', {'posts':posts, 'my_posts':my_posts})
